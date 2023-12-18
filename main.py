3619895b24b8223f3da94210674d9275b7365f86
Sreeram Kondapalli <skondap4@asu.edu>
2023-12-18 11:09:57 -0700

    managed to optimize the code and finally get the input and search to work

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
+# Initialize the driver
 driver = webdriver.Firefox()
+wait = WebDriverWait(driver, 10)
+
 try:
     driver.get("https://catalog.apps.asu.edu/catalog/classes")
-    time.sleep(5)
-    subject_input = driver.find_element(By.NAME, 'Subject')
+    
+    # Closes the ask for cookies by clicking the close button
+    close_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@aria-label='Close']")))
+    close_button.click()
+    
+    # Enter Input For Subject
+    subject_input = wait.until(EC.element_to_be_clickable((By.NAME, 'subject')))
     subject_input.click()
     subject_input.send_keys('CSE')
-    time.sleep(5)
     
-    number_input = driver.find_element(By.NAME, 'Number')
+    # Enter Input For Number
+    number_input = wait.until(EC.element_to_be_clickable((By.NAME, 'catalogNbr')))
     number_input.click()
     number_input.send_keys('355')
-    time.sleep(5)
     
-    search_button = driver.find_element(By.NAME, 'search-button')
+    # Click the Search button
+    search_button = wait.until(EC.element_to_be_clickable((By.ID, "search-button")))
     search_button.click()
-    
 
 finally:
     driver.quit()
