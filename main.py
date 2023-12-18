b18a5007521ae286b7159432bbae9625c20a498e
Sreeram Kondapalli <skondap4@asu.edu>
2023-12-18 10:43:22 -0700

    managed to access website

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-time.sleep(10)
-driver.close()
+try:
+    driver.get("https://catalog.apps.asu.edu/catalog/classes")
+    time.sleep(5)
+    subject_input = driver.find_element(By.NAME, 'Subject')
+    subject_input.click()
+    subject_input.send_keys('CSE')
+    time.sleep(5)
+    
+    number_input = driver.find_element(By.NAME, 'Number')
+    number_input.click()
+    number_input.send_keys('355')
+    time.sleep(5)
+    
+    search_button = driver.find_element(By.NAME, 'search-button')
+    search_button.click()
+    
 
-
-#/Users/sreeram/Desktop/ClassNotifier
+finally:
+    driver.quit()
